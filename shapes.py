629c42db4e552de2eb1e7474d8fb20cd37495e77
BrandonAnsbergs <131714759+BrandonAnsbergs@users.noreply.github.com>
2024-05-08 12:00:53 -0600

    added function to shapes.py

---
+++ shapes.py
@@ -0,0 +1,10 @@
+def calculate_triangle_area(height, base):
+    area = 1/2*base*height
+    return area
+
+def main():
+    print("This is our shapes program")
+    print_introduction()
+
+    rectangle_area = calculate_rectangle_area(10,20)
+    
